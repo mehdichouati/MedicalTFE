069ac6ce0d7a065073fd7c90fd1e9648c5793ff1
mehdichouati <mehdicb7@gmail.com>
2026-07-19 18:53:26 +0200

    chore(F2): Docker Postgres, config DB (psycopg v3, sslmode disable)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -68,6 +68,9 @@ DATABASES = {
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'disable',
+        },
     }
 }
 
